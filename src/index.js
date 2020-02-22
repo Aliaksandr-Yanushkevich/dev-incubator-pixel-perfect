21b092f7cfca14d7827d27062e75bc35e4562bec
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2020-02-22 22:03:35 +0300

    feat: add header

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles/reset.scss';
+import './styles/header.scss';
+import './styles/main.scss';
+import './styles/about-us.scss'
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let config = {
+    entry: {
+        index: "./src/index.js",
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].js",
+        publicPath: "dist/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: "/node_modules/"
+            },
+            // {
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     loader: "eslint-loader",
+            //     options: {
+            //         fix: true
+            //     }
+            // },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf)$/,
+                loader: "url-loader",
+                options: {
+                    name: "./fonts/[name].[ext]",
+                    publicPath: "../"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        ...glob.sync("./*.html").map(htmlFile => {
+            return new HtmlWebpackPlugin({
+              filename: path.basename(htmlFile),
+              template: htmlFile
+            });
+          }),
+    ]
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === "development") {
+        config.devtool = "source-map";
+    }
+    return config;
+};
