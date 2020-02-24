deae1142b0be3cdfb8914312f0c8f429f50aa94f
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2020-02-24 15:54:02 +0300

    feat: add img popup

--- src/index.js
+++ src/index.js
@@ -6,3 +6,29 @@ import './styles/works.scss';
 import './styles/team.scss';
 import './styles/contacts.scss';
 import './styles/footer.scss';
+
+window.onload = function () {
+    // Get the modal
+    const modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+
+    const container = document.querySelector('.items');
+    const modalImg = document.getElementById("img1");
+    container.onclick = function (e) {
+        const selectedImg = e.target.closest('img');
+        if (selectedImg) {
+            modal.style.display = "block";
+            modalImg.src = selectedImg.src;
+        }
+        // captionText.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+}
